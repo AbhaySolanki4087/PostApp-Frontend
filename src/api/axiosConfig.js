ee11d527cdbe085dfe8f595e0e530e7fc82595e8
Your Name <you@example.com>
2025-11-12 19:12:18 +0530

    Add connect-mongo dependency

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,19 +1,7 @@
-// src/api/axiosConfig.js
-// import axios from 'axios';
-
-// const api = axios.create({
-  
-//   baseURL: 'https://postsphere-backend-r4mi.onrender.com/api', // backend baseURL
-//   withCredentials: true, // send cookies for session
-// });
-
-
-// export default api;
-// src/api/axiosConfig.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // adjust if needed
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 // Add JWT token to all requests
